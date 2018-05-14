85e47562738b8d9b581ff45cf3bf6a7e84553948
EC2 Default User <goodfornooooooooothing@gmail.com>
2018-05-14 03:59:51 +0000

    bootstrap

--- app/models/studio.rb
+++ app/models/studio.rb
@@ -1,2 +1,13 @@
 class Studio < ApplicationRecord
+  validates :name, presence: true, length: { maximum: 100 }
+  validates :area_id, presence: true
+  validates :station, presence: true, length: { maximum: 50 }
+  validates :address, presence: true, length: { maximum: 200 }
+  validates :business_hours, presence: true, length: { maximum: 100 }
+  validates :day_off, presence: true, length: { maximum: 50 }
+  validates :charge_weekday, presence: true
+  validates :charge_holiday, presence: true
+  validates :room, presence: true
+  validates :url, presence: true, length: { maximum: 200 }
+  validates :tel, presence: true, length: { maximum: 50 }
 end
