88278d730a6abed0584dcbd87d7951c68f1e9aa1
EC2 Default User <goodfornooooooooothing@gmail.com>
2018-05-25 06:09:52 +0000

    ランキング

--- app/models/area.rb
+++ app/models/area.rb
@@ -1,3 +1,14 @@
 class Area < ApplicationRecord
   has_many :studios
+  
+  def self.ranking_method
+    ranking = {}
+    
+    for i in 1..Area.count
+      ranking[Area.find(i).name] = Area.find(i).studios.length
+    end
+    
+    return Hash[ ranking.sort_by{ |_, v| -v } ]
+    
+  end
 end
